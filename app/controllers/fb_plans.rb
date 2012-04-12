e3a2bff6686cad77a44025f4d52969a78533ed89
Thilo Utke <thilo@upstre.am>
2012-04-12 23:15:44 +0200

    added a welcome page to add the page to Facebook

--- app/controllers/fb_plans.rb
+++ app/controllers/fb_plans.rb
@@ -2,19 +2,19 @@
 CobotFb.controller  do
 
   # fb calling
-  post '/' do
+  post '/plans' do
     signed_page_params = auth.parse_signed_request(params[:signed_request])["page"]
     fb_page_id = signed_page_params["id"]
     session[:fb_page_id] = fb_page_id
     if Space.find_by_fb_id(fb_page_id)
-      redirect "/"
+      redirect "/plans"
     else
       @admin = signed_page_params["admin"]
       render "space/new"
     end
   end
 
-  get '/' do
+  get '/plans' do
     if params[:code] #we just authorized us
       oauth_session = oauth_client.auth_code.get_token(params[:code], :redirect_uri => 'https://cobot-fb.herokuapp.com/')
       oauth_session.options[:header_format] = "OAuth %s" # WTF? Don't work otherwise.
