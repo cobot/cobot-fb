646fc7dc54030d58eab558b26c88a15db7ea50a7
Thilo Utke <thilo@upstre.am>
2013-05-16 00:17:11 +0200

    removed unused code and pass fb_page_id to redirect to address removal of third party cookies

--- app/controllers/fb_plans.rb
+++ app/controllers/fb_plans.rb
@@ -2,21 +2,14 @@
 CobotFb.controller  do
   # fb calling
   post '/plans' do
-    logger.info auth.parse_signed_request(params[:signed_request])
     signed_page_params = auth.parse_signed_request(params[:signed_request])["page"]
     fb_page_id = signed_page_params["id"]
     session[:fb_page_id] = fb_page_id
-    if Space.find_by_fb_id(fb_page_id)
-      redirect "/plans"
-    else
-      @admin = signed_page_params["admin"]
-      render "space/new"
-    end
+    redirect "/plans?fb_page_id=#{fb_page_id}"
   end
 
   get '/plans' do
-
-    space = Space.find_by_fb_id! session[:fb_page_id]
+    space = Space.find_by_fb_id! (params[:fb_page_id] || session[:fb_page_id])
     oauth_session = get_oauth_session(space.token)
     @space_json = oauth_session.get("http://www.cobot.me/api/spaces/#{space.space_id}").body
     @plans_json = oauth_session.get("http://#{space.space_id}.cobot.me/api/plans" ).body
