b61a93f5e6a1c458b796f5f62408b016cdd904e7
Thilo Utke <thilo@upstre.am>
2013-06-13 02:36:46 +0200

    threading experiment

--- app/controllers/fb_plans.rb
+++ app/controllers/fb_plans.rb
@@ -11,8 +11,11 @@ CobotFb.controller  do
   get '/plans' do
     space = Space.find_by_fb_id! (params[:fb_page_id] || session[:fb_page_id])
     oauth_session = get_oauth_session(space.token)
-    @space_json = oauth_session.get("http://www.cobot.me/api/spaces/#{space.space_id}").body
+    t1 = Thread.new{
+      @space_json = oauth_session.get("http://www.cobot.me/api/spaces/#{space.space_id}").body
+    }
     @plans_json = oauth_session.get("http://#{space.space_id}.cobot.me/api/plans" ).body
+    t1.join
     render 'space/plans', layout: :facebook
   end
 end
