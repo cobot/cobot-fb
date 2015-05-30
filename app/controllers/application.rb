2d03d78991b925f5424609515e01ba3124288cb0
Thilo Utke <thilo@upstre.am>
2015-05-31 01:03:50 +0200

    removed options support again

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,12 +2,4 @@ CobotFb.controller do
   get '/', :cache => true do
     render "application/index"
   end
-
-  options "*" do
-    response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
-
-    response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
-
-    200
-  end
 end
