f4a4ff42460a8895183abe6b076fefa9bff345c2
Thilo Utke <thilo@upstre.am>
2017-04-29 22:35:25 -0500

    Cache test responses

--- spec/support/billy_setup.rb
+++ spec/support/billy_setup.rb
@@ -1,5 +1,11 @@
 Billy.configure do |c|  
   c.cache = true
+  c.ignore_params = [
+    'https://www.facebook.com/connect/ping',
+  ]
+  c.strip_query_params = true
+  c.persist_cache = true
+  c.cache_path = 'spec/req_cache/'
 end
 
 # need to call this because of a race condition between persist_cache
