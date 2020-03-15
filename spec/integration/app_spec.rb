114f1fd336dd87f7df380bc86d5232465e671ef7
Thilo Utke <thilo@upstre.am>
2020-03-15 21:51:34 +0100

    updated spec

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -1,35 +1,33 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
-describe "user opens app the first time", type: :request do
+describe 'user opens app the first time', type: :request do
   before(:each) do
     @fb_auth = stub_fb_auth
-    @fb_request_params = {'page' => {'id' => '123'}}
+    @fb_request_params = { 'page' => { 'id' => '123' } }
     @fb_auth.stub(parse_signed_request: @fb_request_params)
   end
 
-  describe "from the app start page", type: :feature do
-    it "displays the auth link for the page admin and redirects to auth" do
+  describe 'from the app start page', type: :feature do
+    it 'displays app is discontinues' do
       visit '/'
-
-      click_link 'Get Started'
-
-      URI.parse(current_url).to_s.should =~ %r{https://www.cobot.me/oauth/authorize}
+      expect(page).to have_content('We are sorry!')
     end
   end
 
-  describe "after it is setup on facebook", type: :feature, js: true do
+  describe 'after it is setup on facebook', type: :feature, js: true do
     before(:each) do
-      Space.create! space_id: 'test', fb_id: "123", token: 't0k3n'
+      Space.create! space_id: 'test', fb_id: '123', token: 't0k3n'
       WebMock.allow_net_connect!
     end
 
-    it "shows the plans" do
+    it 'shows the plans' do
       stub_space_response('test')
-      @plan_hash = {name: "My Plan", price_per_cycle: "100.0", cycle_duration: 1,
-        currency: "EUR", description: "basic plan",
-        day_pass_price: "10.0", cancellation_period: 30, hidden: false,
-        time_passes: []
-      }
+      @plan_hash = { name: 'My Plan', price_per_cycle: '100.0', cycle_duration: 1,
+                     currency: 'EUR', description: 'basic plan',
+                     day_pass_price: '10.0', cancellation_period: 30, hidden: false,
+                     time_passes: [] }
       stub_plans_response_for_space('test', [@plan_hash])
 
       fake_fb_post
